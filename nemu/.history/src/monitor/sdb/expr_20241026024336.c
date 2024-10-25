fe8a43fd8b824d2e1d1e766a47cc84e161560d2d
tracer-ysyx <tracer@ysyx.org>
2024-10-26 02:44:02 +0800

    > compile NEMU ysyx_22040000 张三 Linux icse-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:44:02 up  6:37,  2 users,  load average: 0.10, 0.29, 0.27

--- nemu/.history/src/monitor/sdb/expr_20241026024336.c
+++ nemu/.history/src/monitor/sdb/expr_20241026024336.c
@@ -211,7 +211,7 @@ static bool make_token(char *e) {
             strncpy(tokens[nr_token].str, &e[position - substr_len], substr_len);
             nr_token ++;
             break;
-         case 9:{
+         case :{
                         strncpy(tokens[nr_token].str, substr_start_reg, substr_len - 1);
                         tokens[nr_token].str[2]='\0';
                         nr_token++;
