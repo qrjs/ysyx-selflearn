032a15b462324121d8b55c7da7687e5f97ba984d
tracer-ysyx <tracer@ysyx.org>
2024-10-26 17:32:05 +0800

    > compile NEMU ysyx_22040000 张三 Linux icse-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:32:05 up  2:28,  2 users,  load average: 1.21, 0.95, 0.65

--- nemu/.history/src/monitor/sdb/expr_20241026173023.c
+++ nemu/.history/src/monitor/sdb/expr_20241026173023.c
@@ -133,72 +133,96 @@ bool check_parentheses(int p,int q)
   return false;
 }
 
-static bool make_token(char *e) 
-{   for(int i=0;i<65535;i++)
-    {tokens[i].type=0;
-     strcpy(tokens[i].str,"0");}
-    int position = 0;
-    int i;
-    regmatch_t pmatch;
+static bool make_token(char *e) {
+  int position=0;
+  int i;
+  regmatch_t pmatch;
 
-    nr_token = 0;
-    
-    while (e[position] != '\0') 
-    {
-    
-        for (i = 0; i < NR_REGEX; i ++) 
-        {
-            
-            int reg_res = regexec(&re[i], e + position, 1, &pmatch, 0);
-            
-            
-            if (reg_res == 0 && pmatch.rm_so == 0) 
-            {
-                char *substr_start = e + position;
-                char *substr_start_reg = e + position + 1;
-                int substr_len = pmatch.rm_eo;
+  nr_token = 0;
 
+  while (e[position] != '\0') {
+    /* Try all rules one by one. */
+    for (i = 0; i < NR_REGEX; i ++) {
+      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+        char *substr_start = e + position;
+        int substr_len = pmatch.rm_eo;
         
-                position += substr_len;
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+            i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
-            
-                if (rules[i].token_type == TK_NOTYPE) break;
+        position += substr_len;
+        switch (rules[i].token_type) {
+         case 12:
+            tokens[nr_token++].type='+';
+            break;
+         case 13:
+            tokens[nr_token++].type='-';
+            break;
+         case 14:
+            tokens[nr_token++].type='*';
+            break;
+         case 15:
+            tokens[nr_token++].type='/';
+            break;
+         case 256:
+            break;
+         case 3:
+            tokens[nr_token++].type=3;
+            break;
+         case 4:
+            tokens[nr_token++].type=4;
+            break;
+         case 5:
+            tokens[nr_token++].type=5;
+            break;
+         case 6:
+            tokens[nr_token++].type=6;
+            break;
+         case 7:
+            tokens[nr_token++].type=7;
+            break;
+         case 8:
+            tokens[nr_token++].type=8;
+            break;
+         case 10:
+            tokens[nr_token++].type=10;
+            break;
+         case 11:
+            tokens[nr_token++].type=11;
+            break;
 
-                tokens[nr_token].type = rules[i].token_type;
-        
-                switch (rules[i].token_type) 
-                {
-                    case NUM:
-                    case HEX:
-                        strncpy(tokens[nr_token].str, substr_start, substr_len);
-                        tokens[nr_token].str[substr_len]='\0';
-                        break;
-                    case REGISTER:{
-                        strncpy(tokens[nr_token].str, substr_start_reg, substr_len - 1);
-                        tokens[nr_token].str[2]='\0';
-                        break; 
-                    }
-                    default:
-                        break;
-            
-                }
-
-                nr_token++;
-
-                break;
-            }
-        }
-
-        if (i == NR_REGEX) 
-        {
-            printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
-            return false;
-        }
+//special
+         case 1://NUM
+            tokens[nr_token].type=1;
+            strncpy(tokens[nr_token].str,&e[position-substr_len],substr_len);
+            nr_token++;
+            break;
+         case 2://HEX
+            tokens[nr_token].type = 2;
+            strncpy(tokens[nr_token].str, &e[position - substr_len], substr_len);
+            nr_token ++;
+            break;
+         case 9:
+         tokens[nr_token].type=9;
+        strncpy(tokens[nr_token].str, substr_start, substr_len);
+        nr_token++;
+				break;
+         default:
+            printf("i = %d and No rules is com.\n", i);
+            break;
+         }
+       break;
+       
+      }
     }
 
-    return true;
+    if (i == NR_REGEX) {
+      printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+      return false;
+    }
+  }
+  return true;
 }
-
 
 
 uint32_t char2int(char s[]);
@@ -218,8 +242,13 @@ for(int i=0;i<65535;i++)
 //init the register
  for(int i = 0 ; i < tokens_len ; i ++)
     {
-	    if(tokens[i].type == 9 )
-       
+	    if(tokens[i].type == 9 ){
+        if(tokens[i].str[0]=='$'){
+          for(int j=0;j<65535;j++){
+            tokens[i].str[j]=tokens[i].str[j+1];
+          }
+        }
+      
 	    {   
 	        bool flag = true;
 
@@ -238,7 +267,7 @@ for(int i=0;i<65535;i++)
 	        }
 	    }
       }
-    
+    }
 
 //init the hex
 for (int i=0;i<tokens_len;i++)
